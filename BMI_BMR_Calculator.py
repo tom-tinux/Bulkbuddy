506d9421285c2c5b985c35bbb4b19c02d827fd7e
Senzaa <tomemming@gmail.com>
2021-07-19 13:30:12 +0200

    latest commit

--- BMI_BMR_Calculator.py
+++ BMI_BMR_Calculator.py
@@ -1,32 +1,36 @@
-#!/usr/bin/env python3
-
 from tkinter import Tk, Label, Radiobutton, Entry, IntVar, StringVar, Button
 
 def get_sum(event):
-	Height = int(Height_Entry.get())
-	Weight = int(Weight_Entry.get())
+	# Reset values so we can restart our calculations
+	BMI_sumEntry.delete(0, "end")
+	BMR_sumEntry.delete(0, "end")
+	TDEE_sumEntry.delete(0, "end")
+
+	# Get information from table
+	Height = float(Height_Entry.get().replace(",", "."))
+	Weight = float(Weight_Entry.get().replace(",", "."))
 	Age = int(Age_Entry.get())
 	sex_var = int(m_f.get())
 	Exercise_fact = Exercise_factor.get()
 
+	# Calculate the BMI
 	BMI_sum = round((100 * 100 * Weight) / (Height*Height))
-	BMI_sumEntry.delete(0, "end")
 	BMI_sumEntry.insert(0, BMI_sum)
 
+	# Calculate BMR, basex on the sex selecation
 	if sex_var == 1:
 		BMR_sum = (66.5 + (13.75 * Weight) + (5.003 * Height) - (6.755 * Age))
 	elif sex_var == 2:
 		BMR_sum = (655.1 + (9.563 * Weight) + (1.850 * Height) - (4.676 * Age))
+	BMR_sumEntry.insert(0, BMR_sum)
 
-	BMR = round(BMR_sum)
-	BMR_sumEntry.delete(0, "end")
-	BMR_sumEntry.insert(0, BMR)
-
-	TDEE_sum = round((float(BMR) * float(Exercise_fact)))
-	TDEE_sumEntry.delete(0, "end")
+	# Calculate the TDEE values
+	TDEE_sum = round((float(BMR_sum) * float(Exercise_fact)))
 	TDEE_sumEntry.insert(0, TDEE_sum)
 
+# Start the tkinter screen
 root = Tk()
+
 root.title("BMI / BMR / TDEE Calculator v0.1")
 
 Label(root, text="Enter Height (in CM): ").grid(row=0, column=0, sticky='W')
@@ -48,26 +52,26 @@ Radiobutton(root, text="Female", variable=m_f, value=2).grid(row=3, column=2, st
 
 Exercise_factor = StringVar(value="1")
 Label(root, text="How active are you?").grid(row=4, column=0, sticky='W')
-Radiobutton(root, text="Sedentary", variable=Exercise_factor, value=1.2).grid(row=4, column=1, sticky='W')
+Radiobutton(root, text="Sedentary", variable=Exercise_factor, value=1.2).grid(row=5, column=0, sticky='W')
 Radiobutton(root, text="Lightly active", variable=Exercise_factor, value=1.375).grid(row=5, column=1, sticky='W')
-Radiobutton(root, text="Moderately active", variable=Exercise_factor, value=1.55).grid(row=6, column=1, sticky='W')
-Radiobutton(root, text="Very active", variable=Exercise_factor, value=1.725).grid(row=7, column=1, sticky='W')
-Radiobutton(root, text="Extra active", variable=Exercise_factor, value=1.9).grid(row=8, column=1, sticky='W')
+Radiobutton(root, text="Moderately active", variable=Exercise_factor, value=1.55).grid(row=5, column=2, sticky='W')
+Radiobutton(root, text="Very active", variable=Exercise_factor, value=1.725).grid(row=6, column=0, sticky='W')
+Radiobutton(root, text="Extra active", variable=Exercise_factor, value=1.9).grid(row=6, column=1, sticky='W')
 
 submit_Button = Button(root, text="Calculate BMI and BMR")
 submit_Button.bind("<Button-1>", get_sum)
-submit_Button.grid(row=9, column=1)
+submit_Button.grid(row=7, column=1)
 
-Label(root, text="Your BMI is: ").grid(row=10, column=0, sticky='W')
+Label(root, text="Your BMI is: ").grid(row=8, column=0, sticky='W')
 BMI_sumEntry = Entry(root)
-BMI_sumEntry.grid(row=10, column=1)
+BMI_sumEntry.grid(row=8, column=1)
 
-Label(root, text="Your BMR is: ").grid(row=11, column=0, sticky='W')
+Label(root, text="Your BMR is: ").grid(row=9, column=0, sticky='W')
 BMR_sumEntry = Entry(root)
-BMR_sumEntry.grid(row=11, column=1)
+BMR_sumEntry.grid(row=9, column=1)
 
-Label(root, text="Your TDEE is: ").grid(row=12, column=0, sticky='W')
+Label(root, text="Your TDEE is: ").grid(row=10, column=0, sticky='W')
 TDEE_sumEntry = Entry(root)
-TDEE_sumEntry.grid(row=12, column=1)
+TDEE_sumEntry.grid(row=10, column=1)
 
 root.mainloop()
